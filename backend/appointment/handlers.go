b66fd3865c5e9643fb8c910f06c241a5c4802e74
its-me-yps <yps8181@gmail.com>
2024-02-15 14:08:28 +0530

    forward appointment to doc

--- backend/appointment/handlers.go
+++ backend/appointment/handlers.go
@@ -2,16 +2,18 @@ package appointment
 
 import (
 	"net/http"
-	"time"
+	// "time"
 
+	"github.com/bmerchant22/hc_hackathon/auth"
+	"github.com/bmerchant22/hc_hackathon/ws"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
+	// "github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
+// var upgrader = websocket.Upgrader{
+// 	ReadBufferSize:  1024,
+// 	WriteBufferSize: 1024,
+// }
 
 func CreateAppointmentHandler(ctx *gin.Context) {
 	var appointment *Appointment
@@ -26,48 +28,59 @@ func CreateAppointmentHandler(ctx *gin.Context) {
 		return
 	}
 
+	var doctor auth.User
+	if err := db.Where("user_id = ?", appointment.DocId).First(&doctor).Error; err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "DocId not found"})
+		return
+	}
+
+	if err := ws.Notify(appointment, &doctor); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to forward appointment to the doctor"})
+		return
+	}
+
 	ctx.JSON(http.StatusCreated, gin.H{"message": "Appointment created successfully"})
 }
 
-func fetchAppointmentsHandler(ctx *gin.Context) {
-	ws, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	defer ws.Close()
-	var appointments []Appointment
-	err = fetchAppointments(ctx, &appointments)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+// func fetchAppointmentsHandler(ctx *gin.Context) {
+// 	ws, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
+// 	if err != nil {
+// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 		return
+// 	}
+// 	defer ws.Close()
+// 	var appointments []Appointment
+// 	err = fetchAppointments(ctx, &appointments)
+// 	if err != nil {
+// 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 		return
+// 	}
 
-	// Fetch appointments for the first time
-	for _, appointment := range appointments {
-		if err := ws.WriteJSON(appointment); err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-	}
+// 	// Fetch appointments for the first time
+// 	for _, appointment := range appointments {
+// 		if err := ws.WriteJSON(appointment); err != nil {
+// 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 			return
+// 		}
+// 	}
 
-	for {
-		// Fetch new appointments
-		var appointments []Appointment
-		if err := fetchAppointments(ctx, &appointments); err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
+// 	for {
+// 		// Fetch new appointments
+// 		var appointments []Appointment
+// 		if err := fetchAppointments(ctx, &appointments); err != nil {
+// 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 			return
+// 		}
 
-		// Send appointments over WebSocket
-		for _, appointment := range appointments {
-			if err := ws.WriteJSON(appointment); err != nil {
-				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-				return
-			}
-		}
+// 		// Send appointments over WebSocket
+// 		for _, appointment := range appointments {
+// 			if err := ws.WriteJSON(appointment); err != nil {
+// 				ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 				return
+// 			}
+// 		}
 
-		// Sleep for a while before fetching appointments again
-		time.Sleep(5 * time.Second)
-	}
-}
+// 		// Sleep for a while before fetching appointments again
+// 		time.Sleep(5 * time.Second)
+// 	}
+// }
